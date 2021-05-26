08c28b6084d2997996a6af33d4f73c23e3dae28e
nguyenngoc-it <nguyenngocit.ma@gmail.com>
2021-05-26 17:05:22 +0700

    tuan 3

---
+++ case study/script.js
@@ -0,0 +1,29 @@
+/*
+let products = [
+  ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/1.jpg" ],
+      ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/2.jpg" ],
+      ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/3.jpg" ],
+      ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/4.jpg" ],
+      ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/5.jpg" ],
+      ['Rolex Datejust 31mm Stainless Steel', 200000000, src="img/6.jpg" ]
+
+];
+
+function listProduct() {
+    let str = '';
+    for (let i = 0; i < products.length; i++) {
+        str += `
+              <div class="card">
+                <img src="${products[i][2]}" alt="Denim Jeans" style="width:100%">
+                <h1 id="name-product"><i class="fas fa-coffee"></i>
+${products[i][0]}</h1>
+                <p class="price">${products[i][1]} VNĐ</p>
+                <p><button>Add to Cart</button></p>
+              </div>
+    `;
+    }
+
+    document.getElementById('products').innerHTML = str;
+}
+
+listProduct();*/
